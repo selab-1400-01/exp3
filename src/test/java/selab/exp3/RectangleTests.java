9e0b378bb3b1f25de6d078eb4e18e8d6ab54b6d1
Mahdi Erfanian <mahdi.erfanian@divar.ir>
2021-11-02 12:57:56 +0330

    Add new test

--- src/test/java/selab/exp3/RectangleTests.java
+++ src/test/java/selab/exp3/RectangleTests.java
@@ -11,4 +11,16 @@ public class RectangleTests {
         float area = rectangle.computeArea();
         Assert.assertEquals(50, area, 1e-10);
     }
+
+    @Test
+    public void computeArea_ZeroHeightOrWidth_ReturnsZeroArea() {
+        Rectangle rectangle = new Rectangle(0, 5);
+        float area = rectangle.computeArea();
+        Assert.assertEquals(0, area, 1e-10);
+
+        Rectangle rectangle = new Rectangle(10, 0);
+        area = rectangle.computeArea();
+        Assert.assertEquals(0, area, 1e-10);
+    }
+
 }
