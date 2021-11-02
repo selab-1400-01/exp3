cb703487fa2676017c27c68b4d3e75d12f482c76
momt99 <mohammad.omt99@yahoo.com>
2021-11-02 13:45:32 +0330

    Fix mistakes in tests

--- src/test/java/selab/exp3/SquareTests.java
+++ src/test/java/selab/exp3/SquareTests.java
@@ -1,13 +1,33 @@
 package selab.exp3;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SquareTests {
+    Square square;
+
+    @Before
+    public void setup() {
+        this.square = new Square();
+    }
+
     @Test
     public void computeArea_SampleSide_ReturnsSquareOfSide() {
         Square square = new Square(5);
         float area = square.computeArea();
         Assert.assertEquals(25, area, 1e-10);
+    }
+
+    @Test
+    public void getHeight_WhenWidthIsSet_ReturnsWidth() {
+        this.square.setWidth(10);
+        Assert.assertEquals(10, this.square.getHeight(), 1e-10);
+    }
+
+    @Test
+    public void getWidth_WhenHeightIsSet_ReturnsHeight() {
+        this.square.setHeight(20);
+        Assert.assertEquals(20, this.square.getWidth(), 1e-10);
     }
 }
