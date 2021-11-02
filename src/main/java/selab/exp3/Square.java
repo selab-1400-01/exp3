ac7eb03a2b45df68f4db0f4f3fba0da97a65ba3a
momt99 <mohammad.omt99@yahoo.com>
2021-11-02 13:46:18 +0330

    Add support for setting width and height

--- src/main/java/selab/exp3/Square.java
+++ src/main/java/selab/exp3/Square.java
@@ -4,4 +4,20 @@ public class Square extends Rectangle {
     public Square(float side) {
         super(side, side);
     }
+
+    public Square() {
+
+    }
+
+    @Override
+    public void setHeight(float height) {
+        super.setHeight(height);
+        super.setWidth(height);
+    }
+
+    @Override
+    public void setWidth(float width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
 }
